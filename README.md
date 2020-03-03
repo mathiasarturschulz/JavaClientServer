1b8eb57c04fefa361db15afcbdaee523bfd2acf4
Mathias Artur Schulz <MathiasSchulz34@gmail.com>
2020-03-03 08:44:42 -0300

    [UPD] Testando a conexão entre cliente e servidor, com o cliente rodando de forma iterativa

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # JavaClientServer
+
+Exemplo de aplicação cliente-servidor
+
+### Executar o servidor
+
+Na pasta server, digite:
+
+`docker-compose up`
+
+### Executar o cliente
+
+Na pasta client, digite:
+
+`docker-compose run --rm java-client`
--- client/app/Main.java
+++ client/app/Main.java
@@ -3,24 +3,42 @@ import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
 import java.util.Scanner;
+import java.net.InetAddress;
 
 public class Main
 {
 
-    public static void main(String[] args)
+    public static void main(String[] args) throws Exception
     {
         System.out.println("Tryng to connect...");
 
-        try (Socket client = new Socket("192.168.100.157", 3333)){
+        // ALTERAR O IP PARA O IP ATUAL DO COMPUTADOR - wlp2s0
+        try (Socket client = new Socket("172.16.0.213", 3333)){
             System.out.println("Connected.");
             BufferedWriter buffer = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
             
-            buffer.write("TESTE ");
-            buffer.write("TESTE 2 ");
-            buffer.write("TESTE 3 ");
+            Scanner s = new Scanner(System.in);
+            System.out.println("Informe uma frase para o servidor: ");
+            String frase = s.nextLine();
+
+            System.out.println("Sua frase:  " + frase);
+            buffer.write(frase);
+            buffer.write("Teste de envio de mensagem ");
             buffer.flush();
+            System.out.println("Encerrando... ");
         }catch (IOException e) {
             e.printStackTrace();
         }
     }
+
+    // /**
+    //  * Pega o IP atual utilizado no computador
+    //  */
+    // private static String getIP()
+    // {
+    //     InetAddress inetAddress = InetAddress.getLocalHost();
+    //     // System.out.println("IP Address:- " + inetAddress.getHostAddress());
+    //     // System.out.println("Host Name:- " + inetAddress.getHostName());
+    //     return inetAddress.getHostAddress();
+    // }
 }
